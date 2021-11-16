b76aaff002820b46893e55c5849f41f83f6eb436
dkettner <kettner_dennis@web.de>
2021-11-16 14:06:37 +0100

    Add simple version for esp32-cam tinyML testing

---
+++ src/esp32-cam/smartForestTensorflowTesting/smartForestTensorflowTesting.ino
@@ -0,0 +1,197 @@
+/*********
+  Rui Santos
+  Complete project details at https://RandomNerdTutorials.com/esp32-cam-take-photo-save-microsd-card
+  
+  IMPORTANT!!! 
+   - Select Board "AI Thinker ESP32-CAM"
+   - GPIO 0 must be connected to GND to upload a sketch
+   - After connecting GPIO 0 to GND, press the ESP32-CAM on-board RESET button to put your board in flashing mode
+  
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files.
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+
+  ----------------------------------------------------------------------------------------------------------------
+
+  Modified to include EloquentTinyML:
+  https://eloquentarduino.github.io/2020/01/easy-tinyml-on-esp32-and-arduino/
+  by Simone Salerno
+  
+*********/
+
+// sine_model.h contains the array you exported from Python with xxd or tinymlgen
+#include "sine_model.h"
+
+#include "esp_camera.h"
+#include "Arduino.h"
+#include "FS.h"                // SD Card ESP32
+#include "SD_MMC.h"            // SD Card ESP32
+#include "soc/soc.h"           // Disable brownour problems
+#include "soc/rtc_cntl_reg.h"  // Disable brownour problems
+#include "driver/rtc_io.h"
+#include <EEPROM.h>            // read and write from flash memory
+#include <EloquentTinyML.h>
+#include <eloquent_tinyml/tensorflow.h>
+
+// define the number of bytes you want to access
+#define EEPROM_SIZE             4
+#define PICTURE_INDEX_ADDRESS   0
+
+// Pin definition for CAMERA_MODEL_AI_THINKER
+#define PWDN_GPIO_NUM     32
+#define RESET_GPIO_NUM    -1
+#define XCLK_GPIO_NUM      0
+#define SIOD_GPIO_NUM     26
+#define SIOC_GPIO_NUM     27
+
+#define Y9_GPIO_NUM       35
+#define Y8_GPIO_NUM       34
+#define Y7_GPIO_NUM       39
+#define Y6_GPIO_NUM       36
+#define Y5_GPIO_NUM       21
+#define Y4_GPIO_NUM       19
+#define Y3_GPIO_NUM       18
+#define Y2_GPIO_NUM        5
+#define VSYNC_GPIO_NUM    25
+#define HREF_GPIO_NUM     23
+#define PCLK_GPIO_NUM     22
+
+// tensorflow defines
+#define N_INPUTS 1
+#define N_OUTPUTS 1
+#define TENSOR_ARENA_SIZE 2*1024  // in future projects you may need to tweak this value: it's a trial and error process
+
+Eloquent::TinyML::TensorFlow::TensorFlow<N_INPUTS, N_OUTPUTS, TENSOR_ARENA_SIZE> tf;
+
+void resetEeprom(int range) {
+  for (int eepromAddress = 0; eepromAddress < range; eepromAddress++) {
+    EEPROM.write(eepromAddress, 0);
+  }
+  EEPROM.commit();
+  return;
+}
+
+void setup() {
+  WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0); //disable brownout detector
+ 
+  Serial.begin(115200);
+  Serial.println();
+  
+  camera_config_t config;
+  config.ledc_channel = LEDC_CHANNEL_0;
+  config.ledc_timer = LEDC_TIMER_0;
+  config.pin_d0 = Y2_GPIO_NUM;
+  config.pin_d1 = Y3_GPIO_NUM;
+  config.pin_d2 = Y4_GPIO_NUM;
+  config.pin_d3 = Y5_GPIO_NUM;
+  config.pin_d4 = Y6_GPIO_NUM;
+  config.pin_d5 = Y7_GPIO_NUM;
+  config.pin_d6 = Y8_GPIO_NUM;
+  config.pin_d7 = Y9_GPIO_NUM;
+  config.pin_xclk = XCLK_GPIO_NUM;
+  config.pin_pclk = PCLK_GPIO_NUM;
+  config.pin_vsync = VSYNC_GPIO_NUM;
+  config.pin_href = HREF_GPIO_NUM;
+  config.pin_sscb_sda = SIOD_GPIO_NUM;
+  config.pin_sscb_scl = SIOC_GPIO_NUM;
+  config.pin_pwdn = PWDN_GPIO_NUM;
+  config.pin_reset = RESET_GPIO_NUM;
+  config.xclk_freq_hz = 20000000;
+  config.pixel_format = PIXFORMAT_JPEG; 
+  
+  if(psramFound()){
+    config.frame_size = FRAMESIZE_UXGA; // FRAMESIZE_ + QVGA|CIF|VGA|SVGA|XGA|SXGA|UXGA
+    config.jpeg_quality = 10;
+    config.fb_count = 2;
+  } else {
+    config.frame_size = FRAMESIZE_SVGA;
+    config.jpeg_quality = 12;
+    config.fb_count = 1;
+  }
+  
+  // Init Camera
+  esp_err_t err = esp_camera_init(&config);
+  if (err != ESP_OK) {
+    Serial.printf("Camera init failed with error 0x%x", err);
+    return;
+  }
+  
+  //Serial.println("Starting SD Card");
+  if(!SD_MMC.begin()){
+    Serial.println("SD Card Mount Failed");
+    return;
+  }
+  
+  uint8_t cardType = SD_MMC.cardType();
+  if(cardType == CARD_NONE){
+    Serial.println("No SD Card attached");
+    return;
+  }
+
+  // use fb for tensorflow, contains all picture data
+  camera_fb_t * fb = NULL;
+  
+  // Take Picture with Camera
+  fb = esp_camera_fb_get();  
+  if(!fb) {
+    Serial.println("Camera capture failed");
+    return;
+  }
+  // initialize EEPROM with predefined size
+  EEPROM.begin(EEPROM_SIZE);
+  unsigned long pictureIndex = EEPROM.readULong(PICTURE_INDEX_ADDRESS);
+  EEPROM.writeULong(PICTURE_INDEX_ADDRESS, pictureIndex + 1);
+  EEPROM.commit(); // EEPROM.end(); ???
+  //resetEeprom(EEPROM_SIZE); // uncomment if needed
+
+  // Path where new picture will be saved in SD Card
+  String path = "/picture" + String(pictureIndex) +".jpg";
+
+  fs::FS &fs = SD_MMC; 
+  Serial.printf("Picture file name: %s\n", path.c_str());
+  
+  File file = fs.open(path.c_str(), FILE_WRITE);
+  if(!file){
+    Serial.println("Failed to open file in writing mode");
+  } 
+  else {
+    file.write(fb->buf, fb->len); // payload (image), payload length
+    Serial.printf("Saved file to path: %s\n", path.c_str());
+  }
+  file.close();
+
+  // Turns off the ESP32-CAM white on-board LED (flash) connected to GPIO 4
+  pinMode(4, OUTPUT);
+  digitalWrite(4, LOW);
+  rtc_gpio_hold_en(GPIO_NUM_4);
+
+  // tensorflow -> sine model
+  tf.begin(sine_model);
+  if (!tf.isOk()) {
+    Serial.print("ERROR: ");
+    Serial.println(tf.getErrorMessage());
+  }
+  for (float i = 0; i < 10; i++) {
+    // pick x from 0 to PI
+    float x = 3.14 * i / 10;
+    float y = sin(x);
+    float input[1] = { x };
+    float predicted = tf.predict(input);
+
+    Serial.print("sin(");
+    Serial.print(x);
+    Serial.print(") = ");
+    Serial.print(y);
+    Serial.print("\t predicted: ");
+    Serial.println(predicted);
+  }
+  
+  esp_camera_fb_return(fb); // free frame buffer in psram
+  
+  Serial.println("Going to sleep now");
+  delay(100);
+  esp_deep_sleep_start();
+}
+
+void loop() {}
